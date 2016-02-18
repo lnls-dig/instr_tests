755ad2ceea39e9f34661ed3eba1e787698746f84
Vitor F. Ferreira <vfinotti@gmail.com>
2016-02-18 11:27:32 -0200

    Added agilent_e5061b instrument

---
+++ instruments/vna/agilent_e5061b.py
@@ -0,0 +1,144 @@
+# define specific class for Agilent E5061B Network Analyzer
+
+import visa
+
+class AgilentE5061B:
+    """Class used to send commands and acquire data from the Agilent E5061B vector network analyzer.
+    """
+
+
+
+    def __init__(self, rm,  ip):
+        """Class constructor. Here the socket connection to the instrument is initialized. The
+        argument required, a string, is the IP adress of the instrument."""
+
+        vna_ip = ip
+
+        self.vna_socket = rm.open_resource('TCPIP::' + vna_ip + '::inst0::INSTR')
+
+        #self.vna_socket.send(b":SYST:PRES\n")
+        #self.vna_socket.send(b":DISP:WIND1:TRAC1:Y:RLEV -40\n")
+        #self.vna_socket.send(b":DISP:WIND1:TRAC1:Y:PDIV 15\n")
+        #self.vna_socket.send(b":SENS1:SWE:TIME:AUTO ON\n")
+        #self.vna_socket.send(b":SENS1:SWE:POIN 1601\n")
+        #self.vna_socket.send(b":SENS1:SWE:TYPE LIN\n")
+        #self.vna_socket.send(b":SOUR1:POW:GPP 0.0\n")
+
+        self.vna_socket.write(":SYST:PRES")
+        self.vna_socket.write(":DISP:WIND1:TRAC1:Y:RLEV -40")
+        self.vna_socket.write(":DISP:WIND1:TRAC1:Y:PDIV 15")
+        self.vna_socket.write(":SENS1:SWE:TIME:AUTO ON")
+        self.vna_socket.write(":SENS1:SWE:POIN 1601")
+        self.vna_socket.write(":SENS1:SWE:TYPE LIN")
+        self.vna_socket.write(":SOUR1:POW:GPP 0.0")
+
+
+    def query(self, text):
+        """Return the output of the query on text"""
+        return self.vna_socket.query(text)
+
+    def write(self, text):
+        """Sends a command to the instrument."""
+        self.vna_socket.write(text)
+        #time.sleep(SLEEP_TIME)
+        return
+
+    def get_frequency_data(self):
+        """Get the list of frequencies of the instrument sweep, returning a sequence of floating
+           point numbers."""
+        self.vna_socket.write(":SENS1:FREQ:DATA?\n")
+        frequency_data = ""
+        while (frequency_data[len(frequency_data) - 1:] != "\n"):
+            frequency_data += self.vna_socket.recv(1024)
+        frequency_data = frequency_data[:len(frequency_data) - 1].split(b",")
+        frequency_data = [float(i) for i in frequency_data]
+        return(frequency_data)
+
+    def get_s11_data(self):
+        """Get the S11 trace data, returning a sequence of floating point numbers."""
+        self.vna_socket.write(":CALC1:PAR1:DEF S11")
+        #time.sleep(SLEEP_TIME)
+        #return self.vna_socket.query(":CALC1:DATA:FDAT?")
+        self.vna_socket.write(":CALC1:DATA:FDAT?")
+        s11_data = ""
+        while (s11_data[len(s11_data) - 1:] != b"\n"):
+            s11_data += self.vna_socket.read_raw()
+        s11_data = s11_data[:len(s11_data) - 1].split(b",")
+        s11_data = s11_data[::2]
+        s11_data = [round(float(i),2) for i in s11_data]
+        return(s11_data)
+
+    def get_s12_data(self):
+        """Get the S12 trace data, returning a sequence of floating point numbers."""
+        self.vna_socket.write(":CALC1:PAR1:DEF S12")
+        #time.sleep(SLEEP_TIME)
+        #return self.vna_socket.query(":CALC1:DATA:FDAT?")
+        self.vna_socket.write(":CALC1:DATA:FDAT?")
+        s12_data = ""
+        while (s12_data[len(s12_data) - 1:] != b"\n"):
+            s12_data += self.vna_socket.read_raw()
+        s12_data = s12_data[:len(s12_data) - 1].split(b",")
+        s12_data = s12_data[::2]
+        s12_data = [round(float(i),2) for i in s12_data]
+        return(s12_data)
+
+    def get_s21_data(self):
+        """Get the S21 trace data, returning a sequence of floating point numbers."""
+        self.vna_socket.write(":CALC1:PAR1:DEF S21")
+        #time.sleep(SLEEP_TIME)
+        #return self.vna_socket.query(":CALC1:DATA:FDAT?")
+        self.vna_socket.write(":CALC1:DATA:FDAT?")
+        s21_data = ""
+        while (s21_data[len(s21_data) - 1:] != b"\n"):
+            s21_data += self.vna_socket.read_raw()
+        s21_data = s21_data[:len(s21_data) - 1].split(b",")
+        s21_data = s21_data[::2]
+        s21_data = [round(float(i),2) for i in s21_data]
+        return(s21_data)
+
+    def get_s22_data(self):
+        """Get the S22 trace data, returning a sequence of floating point numbers."""
+        self.vna_socket.write(":CALC1:PAR1:DEF S22")
+        #time.sleep(SLEEP_TIME)
+        #return self.vna_socket.query(":CALC1:DATA:FDAT?")
+        self.vna_socket.write(":CALC1:DATA:FDAT?")
+        s22_data = ""
+        while (s22_data[len(s22_data) - 1:] != b"\n"):
+            s22_data += self.vna_socket.read_raw()
+        s22_data = s22_data[:len(s22_data) - 1].split(b",")
+        s22_data = s22_data[::2]
+        s22_data = [round(float(i),2) for i in s22_data]
+        return(s22_data)
+
+    def set_marker_frequency(self,value):
+        """set the center frequency of the VNA"""
+        self.vna_socket.write(":CALC1:MARK1:X " + str(value))
+
+    def get_marker_value(self,marker):
+        """get the value of the marker 1 """
+        #self.vna_socket.write(":CALC1:MARK" + str(marker) + ":Y?")
+        #ans= self.vna_socket.read_raw()
+        ans = self.vna_socket.query(":CALC1:MARK" + str(marker) + ":Y?")
+        index = ans.find(',')
+        ans = ans[:index].strip()
+        return(ans)
+
+    def set_center_frequency(self,value):
+        """set the center frequency of the VNA"""
+        freq = int(value*1000000)
+        self.vna_socket.write(":SENS1:FREQ:CENT " + str(freq))
+        return
+
+    def set_span(self,value):
+        """set the span of the VNA, in MHz"""
+        freq = int(value*1000000)
+        self.vna_socket.write(":SENS1:FREQ:SPAN " + str(freq))
+        return
+
+    def set_power(self, power):
+        self.vna_socket.write(":SOUR1:POW:GPP " + str(power))
+        return
+
+    #def close_connection(self):
+    #    """Close the socket connection to the instrument."""
+    #    self.vna_socket.close()
