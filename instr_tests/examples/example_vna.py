d6adbcdd627605e91dfa3b71567f1df83a9d0b6f
Vitor F. Ferreira <vfinotti@gmail.com>
2016-03-03 15:04:57 -0300

    Updated example_vna.py to use the package as a native package

--- instr_tests/examples/example_vna.py
+++ instr_tests/examples/example_vna.py
@@ -8,22 +8,16 @@ import visa
 import matplotlib.pyplot as pyplot
 import skrf as rf
 
-cmd_folder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../")))
-
-
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
 
 #############################
 # Importing local libraries #
 #############################
 
-from instruments.vna.agilent_e5061b import AgilentE5061B
-from functions.vna.mag_lvl_test import mag_lvl_test
+import instr_tests as instr
 
 
 try:
-    vna = AgilentE5061B("10.0.18.54")
+    vna = instr.instruments.vna.AgilentE5061B("10.0.18.54")
 except:
     sys.stdout.write("\nUnable to reach the vector network analyzer (Agilent E5061B) through the " +
                      "network. Exiting...\n\n")
@@ -48,8 +42,9 @@ pyplot.savefig('wave1.png')
 #zone_freq = [100e3, 200e6, 450e6, 550e6, 800e6, 1e9]
 #zone_type = [-1, 0, 1, 0, -1]
 #zone_mag = [-60, 0, -60, 0,-40]
+#s21 = vna.get_s21_data()
 
-#print(mag_lvl_test(freq,s21, zone_freq, zone_type, zone_mag))
+#print(instr.functions.vna.mag_lvl_test(freq,s21, zone_freq, zone_type, zone_mag))
 #print(vna.get_reflection_impedance())
 
 #vna.save_csv("SLOG_s11")
