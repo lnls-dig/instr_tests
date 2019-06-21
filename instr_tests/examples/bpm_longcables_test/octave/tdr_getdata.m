90b23f932fa977b3a79d1ba05bab959b652bec27
Daniel Tavares <daniel.tavares@lnls.br>
2019-06-21 14:08:38 -0300

    bpm_longcables_test/octave/*: use waveform source name instead of channel number

--- instr_tests/examples/bpm_longcables_test/octave/tdr_getdata.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_getdata.m
@@ -1,15 +1,12 @@
-function data = tdr_getdata(fid, tdropt, ch)
+function data = tdr_getdata(fid, tdropt, source)
 
-data = [];
-for i=1:length(ch)
-    % Select trace
-    vxi11_write(fid, sprintf(':WAV:SOUR CHAN%d', ch(i)));
-    pause(tdropt.sleep_write);
-    
-    % Ask for trace data
-    vxi11_write(fid, ':WAV:DATA?');
-    pause(tdropt.sleep_query);
-    
-    data_temp = textscan(char(vxi11_read(fid, tdropt.nbytes2read)), '%f,');
-    data = [data data_temp{1}];        
-end
+% Select trace
+vxi11_write(fid, sprintf(':WAV:SOUR %s', source));
+pause(tdropt.sleep_write);
+
+% Ask for trace data
+vxi11_write(fid, ':WAV:DATA?');
+pause(tdropt.sleep_query);
+
+data = textscan(char(vxi11_read(fid, tdropt.nbytes2read)), '%f,');
+data = data{1};
--- instr_tests/examples/bpm_longcables_test/octave/tdr_getrefplane.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_getrefplane.m
@@ -1,4 +1,4 @@
-function refplane = tdr_getrefplane(fid, tdropt, ch)
+function refplane = tdr_getrefplane(fid, tdropt, source)
   
-vxi11_write(fid, sprintf(':TDR:RESPonse%d:RPL?',ch));
+vxi11_write(fid, sprintf(':TDR:%s:RPL?', source));
 refplane = str2double(char(vxi11_read(fid, tdropt.nbytes2read)));
