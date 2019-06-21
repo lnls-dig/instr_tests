bab29d0d595d3cb779a89638ad06eaac5533e2ae
Daniel Tavares <daniel.tavares@lnls.br>
2019-06-21 09:00:20 -0300

    tdr_getrefplane.m: fix typo on function name

--- instr_tests/examples/bpm_longcables_test/octave/tdr_getrefplane.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_getrefplane.m
@@ -1,4 +1,4 @@
-function refplane = tdr_getreplane(fid, tdropt, ch)
+function refplane = tdr_getrefplane(fid, tdropt, ch)
   
 vxi11_write(fid, sprintf(':TDR:RESPonse%d:RPL?',ch));
 refplane = str2double(char(vxi11_read(fid, tdropt.nbytes2read)));
