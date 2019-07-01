e6bae188e2eef7e2a8d932b654ba60829cd44a84
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-01 17:56:35 -0300

    tdr_distest_plot.m: trivial fixes

--- instr_tests/examples/bpm_longcables_test/octave/tdr_distest_plot.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_distest_plot.m
@@ -1,4 +1,4 @@
-function [hplot, ref_dly] = tdr_distest_plot(t, data, dly_idx, diel, n, clr, call_type, hplot)
+function hplot = tdr_distest_plot(t, data, dly_idx, diel, n, clr, call_type, hplot)
 
 if nargin < 7 || isempty(call_type)
     call_type = 'first';
@@ -35,6 +35,6 @@ end
 
 xlabel(sprintf('Distance relative to %0.3f m [mm]', ref_dist), 'FontSize', 16);
 for i=1:size(data,2)
-    leg{i} = sprintf('Cable #%i, \\Deltat_{%d\\rightarrow%d}: %0.0f mm\n', i, ref_i, i, dist(dly_idx(i)));
+    leg{i} = sprintf('Cable #%d, \\Deltat_{%d\\rightarrow%d}: %0.0f mm\n', i, ref_i, i, dist(dly_idx(i)));
 end
 legend(leg, 'Location', 'NorthWest')
