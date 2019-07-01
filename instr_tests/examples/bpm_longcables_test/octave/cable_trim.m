9a43cd14d6c89c5a802bf567abc3ca9925a61a90
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-01 16:24:32 -0300

    cable_trim.m: enhance user interaction and add results saving to file

--- instr_tests/examples/bpm_longcables_test/octave/cable_trim.m
+++ instr_tests/examples/bpm_longcables_test/octave/cable_trim.m
@@ -12,7 +12,19 @@ clr = [ ...
     0    0    0
     ];
 
-fid = vxi11(tdr_ip);
+cable_name = input('Enter the cable name and continue [temp]: ','s');
+if isempty(cable_name)
+    cable_name = 'temp';
+end
+
+fprintf('Starting cable trimming procedure...\n');
+
+try
+    fid = vxi11(tdr_ip);
+catch err
+    error('Could not connect to TDR device at IP address ''%s''', tdr_ip);
+end
+
 tdropt = tdr_options;
 
 % Retrieve TDR reference plane in seconds
@@ -23,47 +35,60 @@ t_orig = tdr_gettime(fid, tdropt);
 t = t_orig-refplane;
 
 % First length measurement for all cables
-dly_idx = zeros(4,1);
-deriv = zeros(4,1);
-cable_dly = zeros(4,1);
 data = [];
 for i=1:4
+    fprintf('Measuring cable %d...', i);
     swbox_setsw(swbox_ip, 'A', i);
     pause(2);
     data = [data tdr_getdata(fid, tdropt, tdr_source)];
+    fprintf('done.\n');
 end
 [dly_idx, deriv] = tdr_dlyest(data, dlyest_navg);
-[hplot, ref_dly] = tdr_distest_plot(t, data, dly_idx, cable_diel, npts_plot, clr);
+hplot = tdr_distest_plot(t, data, dly_idx, cable_diel, npts_plot, clr);
 
 % Update each length measurement at user's request
 last_ch = i;
 while true
-    r = input(sprintf('Choose cable measurement to be updated (1/2/3/4) [default = %d (last value)] and press <Enter>. Press ''q'' + <Enter> to quit): \n', last_ch),'s');
+    r = input(sprintf('Choose cable measurement to be updated (1/2/3/4) or ''q'' to quit [%d]: ', last_ch),'s');
     switch r
         case 'q'
             break
         otherwise
             if isempty(r)
                 i = last_ch;
-                fprintf('Selecting default (cable #%d)...\n',i);
             else
                 rnum = str2double(r);
                 if rnum >= 1 && rnum <= 4 && mod(rnum,1) == 0
                     i = rnum;
-                    fprintf('Selected cable #%d.\n',i);
                 else
-                    fprintf('Wrong option. Choose among 1/2/3/4.\n');
+                    fprintf('Wrong option. Choose among 1/2/3/4/q.\n');
                     continue
                 end
             end
     end
     
+    fprintf('Measuring cable %d... ', i);
     swbox_setsw(swbox_ip, 'A', i);
     pause(2);
     data(:,i) = tdr_getdata(fid, tdropt, tdr_source);
+    fprintf('done.\n');
+    
     [dly_idx, deriv] = tdr_dlyest(data, dlyest_navg);
     tdr_distest_plot(t, data, dly_idx, cable_diel, npts_plot, [], 'update', hplot);
     last_ch = i;
 end
 
 vxi11_close(fid);
+
+% Save results to JSON file
+filename = sprintf('cable_trim_%s_%s.json', cable_name, datestr(now, 'yyyy-mm-dd_HH-MM-SS'));
+
+fprintf('Saving results to file ''%s''...\n\n', filename);
+
+result.refplane = refplane;
+result.t = t';
+result.data = data';
+result.cable_dielectric = cable_diel;
+
+opt.FileName = filename;
+savejson('', result, opt);
