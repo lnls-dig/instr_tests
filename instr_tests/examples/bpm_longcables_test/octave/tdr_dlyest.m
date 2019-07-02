478ebdc277104b1d082d91a75aea07890cea53c4
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-02 13:07:52 -0300

    tdr_dlyest.m: add option for using second derivative for estimation

--- instr_tests/examples/bpm_longcables_test/octave/tdr_dlyest.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_dlyest.m
@@ -1,4 +1,4 @@
-function [dly, deriv] = tdr_dlyest(data, arg2)
+function [dly, deriv] = tdr_dlyest(data, arg2, nderiv)
 
 if isscalar(arg2)
     n = arg2;
@@ -8,7 +8,16 @@ else
     n = (length(fircoeff)+1)/2+1;    
 end
  
+if nargin < 3 || isempty(nderiv)
+    nderiv = 1;
+end
+
 data_smo = filter(fircoeff, 1, data);
 data_smo = data_smo(n:end, :);
-[deriv,dly] = max(diff(data_smo));
-dly = dly-1;
+
+if nderiv == 2
+    [deriv,dly] = max(diff(diff(data_smo)));
+else
+    [deriv,dly] = max(diff(data_smo));
+    dly = dly-1;
+end
