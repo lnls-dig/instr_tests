91d4e7c240cdc0f17e94d24da3d7034422942318
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-03 08:04:00 -0300

    tdr_distest_print.m: fix unit on printed text

--- instr_tests/examples/bpm_longcables_test/octave/tdr_distest_print.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_distest_print.m
@@ -3,4 +3,4 @@ function [rel_dist, ref_dist] = tdr_distest_print(cable_dly, diel)
 cable_dist = tdr_time2dist(cable_dly, diel);
 [ref_cable_dist, ref_i] = min(cable_dist);
 cable_dist_rel_mm = round((cable_dist - ref_cable_dist)/1e-3);
-fprintf('Estimated distances (relative to reference distance):\n    Cable #1: %g mm\n    Cable #2: %g mm\n    Cable #3: %g mm\n    Cable #4: %g mm\n\nReference distance (cable #%d): %0.3f mm\n\n', cable_dist_rel_mm(1), cable_dist_rel_mm(2), cable_dist_rel_mm(3), cable_dist_rel_mm(4), ref_i, ref_cable_dist);
+fprintf('Estimated distances (relative to reference distance):\n    Cable #1: %g mm\n    Cable #2: %g mm\n    Cable #3: %g mm\n    Cable #4: %g mm\n\nReference distance (cable #%d): %0.3f m\n\n', cable_dist_rel_mm(1), cable_dist_rel_mm(2), cable_dist_rel_mm(3), cable_dist_rel_mm(4), ref_i, ref_cable_dist);
