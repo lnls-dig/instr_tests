c55a88ed17766786768bfdf9513d8953ad93e201
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-05 14:50:14 -0300

    tdr_distest_*.m: improve results report
    Use only mm units for distance and 2 digits after decimal point.

--- instr_tests/examples/bpm_longcables_test/octave/tdr_distest_plot.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_distest_plot.m
@@ -5,7 +5,7 @@ if nargin < 7 || isempty(call_type)
 end
 
 [ref_dly, ref_i] = min(dly);
-ref_dist = tdr_time2dist(ref_dly, diel);
+ref_dist = tdr_time2dist(ref_dly, diel)/1e-3;
 dist = ((tdr_time2dist(t - ref_dly, diel)))/1e-3;
 idx = -ceil(n/3-1):floor(2*n/3);
 dist_cable = tdr_time2dist(dly - ref_dly, diel)/1e-3;
@@ -43,8 +43,8 @@ elseif strcmp(call_type, 'update')
     end
 end
 
-xlabel(sprintf('Distance relative to %0.3f m [mm]', ref_dist), 'FontSize', 16);
+xlabel(sprintf('Distance relative to %0.2f mm [mm]', ref_dist), 'FontSize', 16);
 for i=1:size(data,2)
-    leg{i} = sprintf('Cable #%d, \\Deltat_{%d\\rightarrow%d}: %0.0f mm\n', i, ref_i, i, dist_cable(i));
+    leg{i} = sprintf('Cable #%d, \\Deltat_{%d\\rightarrow%d}: %0.2f mm\n', i, ref_i, i, dist_cable(i));
 end
 legend(leg, 'Location', 'NorthWest')
