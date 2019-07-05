8a7b8d762ed2ac97e45e35da036c883b53be7b1a
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-05 16:19:31 -0300

    tdr_dlyest.m: add new delay estimation method ('edge')
    Detect transient edge by threshold.

--- instr_tests/examples/bpm_longcables_test/octave/tdr_dlyest.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_dlyest.m
@@ -43,4 +43,38 @@ switch method
             dly_idx = dly_idx-1;
         end
         dly = t(dly_idx);
+
+    case 'edge'
+        if isempty(varargin)
+            n = 1000;
+            ninterp = 1;
+            pct =  0.5;
+            pct_tol = 0.01;
+            nstable = 10;
+            dly_guess = zeros(size(data,2), 1);
+        else
+            n = varargin{1};
+            ninterp = varargin{2};
+            pct =  varargin{3};
+            pct_tol = varargin{4};
+            nstable = varargin{5};
+            dly_guess = varargin{6};
+        end
+        Fs = 1/(t(2)-t(1));
+        [data, t] = upsample(data, ninterp, ninterp, 1/Fs, t(1));
+        Fs = 1/(t(2)-t(1));
+
+        data_roi = zeros(n,size(dly_guess));
+        dly = zeros(size(dly_guess));
+        for i=1:length(dly)
+            aux = find(t <= dly_guess(i));
+            dly_idx(i) = aux(end);
+            idx = dly_idx(i) - round(n/2) + (-ceil(n/5-1):floor(4*n/5));
+            t_roi0 = t(idx(1));
+            data_roi(:,i) = data(idx, i);
+            data_roi(:,i) = data_roi(:,i) - mean(data_roi(1:nstable,i));
+            data_roi(:,i) = data_roi(:,i)./mean(data_roi(end-nstable+1:end,i));
+            dly_idx_roi(i) = mean(mean(find(data_roi(:,i) <= pct + pct_tol & data_roi(:,i) >= pct - pct_tol)));
+            dly(i) = t_roi0 + (dly_idx_roi(i)'-1)/Fs;
+        end
 end
