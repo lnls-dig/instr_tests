684e8cd84b7fe436ea51000c509f6f501f454538
Daniel Tavares <daniel.tavares@lnls.br>
2019-07-27 11:06:07 -0300

    tdr_distest_print.m: simplify text

--- instr_tests/examples/bpm_longcables_test/octave/tdr_distest_print.m
+++ instr_tests/examples/bpm_longcables_test/octave/tdr_distest_print.m
@@ -3,4 +3,4 @@ function [rel_dist, ref_dist] = tdr_distest_print(cable_dly, diel)
 cable_dist = tdr_time2dist(cable_dly, diel)/1e-3;
 [ref_cable_dist, ref_i] = min(cable_dist);
 cable_dist_rel_mm = (cable_dist - ref_cable_dist);
-fprintf('Estimated distances (relative to reference distance):\n    Cable #1: %0.2f mm\n    Cable #2: %0.2f mm\n    Cable #3: %0.2f mm\n    Cable #4: %0.2f mm\n\nReference distance (cable #%d): %0.2f mm\n\n', cable_dist_rel_mm(1), cable_dist_rel_mm(2), cable_dist_rel_mm(3), cable_dist_rel_mm(4), ref_i, ref_cable_dist);
+fprintf('Estimated cable lengths (relative):\nCable #1: %0.2f mm\nCable #2: %0.2f mm\nCable #3: %0.2f mm\nCable #4: %0.2f mm\nReference distance (cable #%d): %0.2f mm\n\n', cable_dist_rel_mm(1), cable_dist_rel_mm(2), cable_dist_rel_mm(3), cable_dist_rel_mm(4), ref_i, ref_cable_dist);
